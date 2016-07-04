152f417540ebc474a21dbde0e11097282a5ebbc1
Shabeer M <msg2shabeer@users.noreply.github.com>
2016-07-04 11:03:54 +0530

    Update account_invoice.py

--- account_clean_cancelled_invoice_number/account_invoice.py
+++ account_clean_cancelled_invoice_number/account_invoice.py
@@ -3,11 +3,10 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp.osv import osv
+from openerp import models, fields, api, _
 
-class invoice(osv.osv):
+class invoice(models.Model):
     _inherit = 'account.invoice'
 
-    def clean_internal_number(self, cr, uid, ids, context=None):
-        # We also clean reference for compatibility with argentinian localization
-        self.write(cr, uid, ids, {'internal_number':False,'afip_document_number':False}, context=context)
+    def clean_internal_number(self):
+        self.move_name = False
